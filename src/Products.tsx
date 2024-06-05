05fa05c5d73b70993a89c39cb8ac905c556a7176
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-06-05 16:26:23 +0900

    :sparkles:[add]product details

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,5 +1,6 @@
 import products from '../products.json';
 import './Products.css';
+import ProductsDetail from './ProductsDetail';
 
 function Products() {
   return (
@@ -8,15 +9,20 @@ function Products() {
       <div className="product-list">
         {products.map((product) => (
           <div key={product.id} className="product-card">
-            <img src={product.image_urls[0]} alt={product.name} className="product-image" />
+            <a href="#mayo">
+              <img src={product.image_urls[0]} alt={product.name} className="product-image"/>
+            </a>
             <div className="product-info">
-              <h2 className="product-name">{product.name}</h2>
-              <p className="product-price">¥{product.price}</p>
-              <p className="product-title">{product.title}</p>
+              <a href="#mayo">
+                <h2 className="product-name">{product.name}</h2>
+              </a>
+                <p className="product-price">¥{product.price}</p>
+                <p className="product-title">{product.title}</p>
             </div>
           </div>
-        ))}
+          ))}
       </div>
+      <ProductsDetail/>
     </>
   );
 }

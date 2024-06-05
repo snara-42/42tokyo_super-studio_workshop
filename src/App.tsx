cb9428ed4c642aac1596cb9b479877bb8fecf772
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-06-05 17:09:23 +0900

    :dizzy:[update]nav fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,28 @@ function App() {
   return (
     <Router>
       <div>
-        <nav style={{ borderBottom: "1px solid", marginBottom: "20px" }}>
+        <nav style={{
+          'borderBottom': '1px solid',
+          'marginBottom': '20px',
+          padding: '20px 0',
+          position: 'fixed',
+          width: '100%',
+          top: 0,
+          left: 0,
+          background: '#fff'
+        }}>
+
           <Link to="/">ホーム</Link> |{" "}
-          <Link to="/privacy">プライバシーポリシー</Link>
+          <Link to="/privacy">プライバシーポリシー</Link> <Link to="/" style={{
+          position: 'absolute',
+          right: '20px',
+          top: '20px'
+        }}>カート</Link>
+
         </nav>
         <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/" element={<Products/>}/>
+          <Route path="/privacy" element={<Privacy/>}/>
         </Routes>
       </div>
     </Router>
